abf6ffd28bb04fec286aad8b184428a4b801a879
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-03-08 21:06:24 +0200

    add verification page

---
+++ verification.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/verification.css">
+    <title>Verification Your Email</title>
+</head>
+
+<body>
+    <form action="#">
+        <h4 class="text-center mb-4">Enter your code</h4>
+        <div class="d-flex mb-3">
+            <input type="tel" maxlength="1" pattern="[0-9]" class="form-control">
+            <input type="tel" maxlength="1" pattern="[0-9]" class="form-control">
+            <input type="tel" maxlength="1" pattern="[0-9]" class="form-control">
+            <input type="tel" maxlength="1" pattern="[0-9]" class="form-control">
+            <input type="tel" maxlength="1" pattern="[0-9]" class="form-control">
+            <input type="tel" maxlength="1" pattern="[0-9]" class="form-control">
+        </div>
+        <button type="submit" class="w-100 btn btn-primary">Verify account</button>
+    </form>
+    <script src="js/verification.js"></script>
+</body>
+
+</html>
