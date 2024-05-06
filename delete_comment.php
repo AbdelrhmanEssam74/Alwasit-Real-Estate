e7525d6b8a2f8cd0f1eba49f807e0be891a34df7
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-05-06 03:25:19 +0300

    Updated filenames and variables related to neighborhoods; modified AJAX requests to get neighborhood data; added methods to GeneralClass for updating and getting neighborhood data; updated delete\_comment.php and send\_comment.php to include property\_id

--- delete_comment.php
+++ delete_comment.php
@@ -6,4 +6,13 @@ $comment_obj = new GeneralClass;
 $user_id = $_POST['uID'];
 $comment_id = $_POST['commentID'];
 $delete_query = "DELETE FROM comments WHERE user_id = '$user_id' AND comment_id = '$comment_id'";
-echo $comment_obj->delete($delete_query);
+// echo $comment_obj->delete($delete_query);
+// update number of comments in comments_num for property
+// get the value of the number and decrease it
+$number_of_comments = "SELECT `comments_num` FROM `properties` WHERE `property_id` = '$property_id'";
+$num = $comment_obj->select($number_of_comments);
+$n = $num[0]['comments_num'];
+$n = $n - 1;
+echo $n;
+$update_query = "UPDATE `properties` SET `comments_num` = '$n' WHERE `property_id` = '$property_id'";
+echo $comment_obj->update($update_query);
